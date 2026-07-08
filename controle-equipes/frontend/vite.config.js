f16790ae8267deb0ff93a1ee35499a172c9f5806
Henrique Vale <henriquevale097@gmail.com>
2026-07-08 14:18:52 -0300

    Fix: corrigindo import do plugin react no vite config

--- controle-equipes/frontend/vite.config.js
+++ controle-equipes/frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' // ou seu plugin do react
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    cssMinify: 'esbuild' // <-- Adicione esta linha dentro de build
+    cssMinify: 'esbuild'
   }
 })
