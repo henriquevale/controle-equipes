e07d70a2180c776ee3c4632bae18eeeb4fe94f49
Henrique Vale <henriquevale097@gmail.com>
2026-07-10 07:56:58 -0300

    fix: atualiza URL da API de novo

--- controle-equipes/frontend/src/assets/CadastroFuncionario.jsx
+++ controle-equipes/frontend/src/assets/CadastroFuncionario.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { UserPlus, Save, AlertCircle, RefreshCw, Users, CalendarDays } from 'lucide-react';
 
 // Em vez de: const API_URL = 'http://localhost:3001/api';
-const API_URL = 'https://controle-equipes.onrender.com'; //i
+const API_URL = 'https://controle-equipes.onrender.com/api'; //i
 
 export default function CadastroFuncionario({ usuarioLogado }) {
   const [formData, setFormData] = useState({
