88bb7e29523d0746a659f16fec07a8bef5468539
Henrique Vale <henriquevale097@gmail.com>
2026-08-27 15:08:06 -0300

    ADCIONEI MAIS ATIVIDADES E METERIAIS 2

--- controle-equipes/frontend/src/assets/DiarioObraTecnico.jsx
+++ controle-equipes/frontend/src/assets/DiarioObraTecnico.jsx
@@ -4,8 +4,8 @@ import { Save, AlertCircle, Plus, Trash2, FileText, Package, HardHat, CalendarDa
 import { jsPDF } from 'jspdf';
 import autoTable from 'jspdf-autotable';
 
-//const API_URL = 'http://localhost:3001/api';
-const API_URL = 'https://api-controle-impacto.duckdns.org/api';
+const API_URL = 'http://localhost:3001/api';
+//const API_URL = 'https://api-controle-impacto.duckdns.org/api';
 
 const SERVICOS_PADRONIZADOS = [
  "REMOÇÃO DE TACHA (UN)",
